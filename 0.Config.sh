11c8fdc3b1c66ee4dae87bc05c3fcedd04ae502f
Trevor Young <trev@google.com>
2016-11-17 16:39:25 +0000

    Updated 0.Config.sh to ensure the projectid is provided

--- 0.Config.sh
+++ 0.Config.sh
@@ -1,5 +1,6 @@
 export APPNAME=nodehello
 export CLUSTER=kubeweb1gke
+#Make sure you have the PROJECTID and not the  Project Name or Number
 export PROJECTID=kubeweb1
 export REGION=us-east1
 export ZONE=us-east1-d
