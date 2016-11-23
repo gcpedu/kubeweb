eda13cea3881cc62c38e72bf208e665f63db2d08
Trevor Young <trev@google.com>
2016-11-23 20:41:04 +0000

    Fixed merge error

--- 0.Config.sh
+++ 0.Config.sh
@@ -1,8 +1,6 @@
-export APPNAME=nodehello
-export PROJECTID=kubeweb9
-export CLUSTER=kubeweb1gke
 #Make sure you have the PROJECTID and not the  Project Name or Number
-export PROJECTID=kubeweb1
+export PROJECTID=your_project_id_NOT_NAME
+export CLUSTER=klusta
 export REGION=us-east1
 export ZONE=us-east1-d
-
+export APPNAME=nodehello
